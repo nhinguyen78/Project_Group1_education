77a355104e177ef585c8f93cc3a458b45907f82d
nhinguyen78 <84339738+nhinguyen78@users.noreply.github.com>
2021-08-16 14:56:50 +0700

    edit data generation file
    change data generate.py

--- resources/data-generator.py
+++ resources/data-generator.py
@@ -28,9 +28,9 @@ def create_csv_file_Transaction_Info():
     raw_path = os.path.dirname(__file__)
     with open(f'{raw_path}\CoursesData-{time_stampe}.csv', 'w', newline='') as csvfile:
         fieldnames = ['Index','Transaction_ID','Instructor_Name',
-        'Class_Type', 'Date of Birth', 'Course_Name', 'Gender', 
+        'Date of Birth', 'Course_Name', 'Gender', 
         'Rating_class', 'Student_Name', 'Class_recommendation', 'Email', 
-        'Instructor_rating', 'Instructor_level', 'Final_Score', 'Registered_date']
+        'Instructor_rating', 'Final_Score', 'Registered_date']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         RECORD_COUNT = 2000
         writer.writeheader()
@@ -39,14 +39,21 @@ def create_csv_file_Transaction_Info():
                 {
                     'Index': i,
                     'Transaction_ID': fake.random_int(1,2000),
-                    'Instructor_Name': random.choice(['Erik Hawkins','Lauren Davis','Patrick Duran','Teresa Norris','Chad Giles',
-                    'Matthew Hunter','Mrs. Julia Herman','Kristina Wilson','Ryan Hamilton','Richard Donovan','Nicholas Sanchez','Paul Jones',
-                    'Dr. Kenneth Patterson MD','Toni Williams','Sarah Henry','Heather Hoffman','Zachary Holland','Jennifer Kelly',
-                    'Robert Campbell','Jonathan Fisher']),
+                    'Instructor_Name': random.choice(['Erik Hawkins, Ph.D','Lauren Davis, Master','Patrick Duran, Ph.D','Teresa Norris, Ph.D','Chad Giles, Ph.D',
+                    'Matthew Hunter, Master','Mrs. Julia Herman, Master','Kristina Wilson, Ph.D','Ryan Hamilton, Ph.D','Richard Donovan, Master','Nicholas Sanchez, Ph.D','Paul Jones, Ph.D']),
+                    'Course_Name': random.choice([
+                        'Artificial intelligence course','Machine learning course','Network Security Course','Moral Hacking Course','Website optimization course','Full Stack Developer Course','Web Development Course',
+                        'Php and MySQL','Advanced UI Design','Graphic Design', 
+                        'ONLINE_Artificial intelligence course','ONLINE_Machine learning course','ONLINE_Network Security Course','ONLINE_Moral Hacking Course','ONLINE_Website optimization course','ONLINE_Full Stack Developer Course','ONLINE_Web Development Course',
+                        'ONLINE_Php and MySQL','ONLINE_Advanced UI Design','ONLINE_Graphic Design']),
+                    'Student_Name': fake.name(),
+                    'Gender':random.choice(['Male', 'Female']),
+                    'Email': fake.email(),
+                    'Date_of_Birth': random_date("1/1/2017", "6/1/2021", random.random()),
+                    'Final_Score': fake.random_int(1,10),
                     'Instructor_rating': fake.random_int(1,5),
                     'Rating_class': fake.random_int(1,5),
                     'Class_recommendation':random.choice(['Yes', 'No']),
-                    'Class_Type': random.choice(['Online', 'Offline']),
                     'Registered_date': random_date("1/1/2017", "6/1/2021", random.random())
                     
                 }
