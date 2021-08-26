bdfb84dfdd392efee8ec0c071ae1d87ac3032db2
Minh Hồ <65065691+duy-deliver@users.noreply.github.com>
2021-08-26 22:58:14 +0700

    Fix Code Python
    - Fix Syntax
    - Fix Path File
    - Trigger some data rows

--- resources/data-generator.py
+++ resources/data-generator.py
@@ -21,50 +21,53 @@ def str_time_prop(start, end, time_format, prop):
 def random_date(start, end, prop):
     return str_time_prop(start, end, '%m/%d/%Y', prop)
 
-
+Instructor_Name = ['Erik Hawkins Ph.D','Lauren Davis Master','Patrick Duran Ph.D','Teresa Norris Ph.D','Chad Giles Ph.D',
+                    'Matthew Hunter Master','Mrs. Julia Herman Master','Kristina Wilson Ph.D','Ryan Hamilton Ph.D','Richard Donovan Master','Nicholas Sanchez Ph.D','Paul Jones Ph.D']
 def create_csv_file_Transaction_Info():
     # pdb.set_trace()
     time_stampe = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
     raw_path = os.path.dirname(__file__)
-    with open(f'{raw_path}\CoursesData-{time_stampe}.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Index','Transaction_ID','Instructor_Name',
-        'Date of Birth', 'Course_Name', 'Gender', 
+    with open(f'{raw_path}\RawData.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Transaction_ID','Instructor_Name',
+        'Date_of_Birth', 'Course_Name', 'Gender',
         'Rating_class', 'Student_Name', 'Class_recommendation', 'Email', 
         'Instructor_rating', 'Final_Score', 'Registered_date']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         RECORD_COUNT = 2000
         writer.writeheader()
+        j = 0
         for i in range(RECORD_COUNT):
+            if j >= len(Instructor_Name):
+                j = 0
             writer.writerow(
                 {
-                    'Index': i,
-                    'Transaction_ID': fake.random_int(1,2000),
-                    'Instructor_Name': random.choice(['Erik Hawkins, Ph.D','Lauren Davis, Master','Patrick Duran, Ph.D','Teresa Norris, Ph.D','Chad Giles, Ph.D',
-                    'Matthew Hunter, Master','Mrs. Julia Herman, Master','Kristina Wilson, Ph.D','Ryan Hamilton, Ph.D','Richard Donovan, Master','Nicholas Sanchez, Ph.D','Paul Jones, Ph.D']),
+                    'Transaction_ID': i,
+                    'Instructor_Name': Instructor_Name[j],
+                    'Date_of_Birth': random_date("1/1/2017", "6/1/2021", random.random()),
                     'Course_Name': random.choice([
                         'Artificial intelligence course','Machine learning course','Network Security Course','Moral Hacking Course','Website optimization course','Full Stack Developer Course','Web Development Course',
-                        'Php and MySQL','Advanced UI Design','Graphic Design', 
+                        'Php and MySQL','Advanced UI Design','Graphic Design',
                         'ONLINE_Artificial intelligence course','ONLINE_Machine learning course','ONLINE_Network Security Course','ONLINE_Moral Hacking Course','ONLINE_Website optimization course','ONLINE_Full Stack Developer Course','ONLINE_Web Development Course',
                         'ONLINE_Php and MySQL','ONLINE_Advanced UI Design','ONLINE_Graphic Design']),
+                    'Gender': random.choice(['Male', 'Female']),
+                    'Rating_class': fake.random_int(1, 5),
                     'Student_Name': fake.name(),
-                    'Gender':random.choice(['Male', 'Female']),
+                    'Class_recommendation': random.choice(['Yes', 'No']),
                     'Email': fake.email(),
-                    'Date_of_Birth': random_date("1/1/2017", "6/1/2021", random.random()),
+                    'Instructor_rating': fake.random_int(1, 5),
                     'Final_Score': fake.random_int(1,10),
-                    'Instructor_rating': fake.random_int(1,5),
-                    'Rating_class': fake.random_int(1,5),
-                    'Class_recommendation':random.choice(['Yes', 'No']),
                     'Registered_date': random_date("1/1/2017", "6/1/2021", random.random())
-                    
                 }
-)
+            )
+            j += 1
+
             
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
-DimCourse = f'{dir_path}\\workingFolder\\DimCourse.csv'
-DimInstructor = f'{dir_path}\\workingFolder\\DimInstructor.csv'
-DimStudent = f'{dir_path}\\workingFolder\\DimStudent.csv'
-FactTransaction = f'{dir_path}\\workingFolder\\FactTransaction.csv'
+DimCourse = f'{dir_path}\\DimCourse.csv'
+DimInstructor = f'{dir_path}\\DimInstructor.csv'
+DimStudent = f'{dir_path}\\DimStudent.csv'
+FactTransaction = f'{dir_path}\\FactTransaction.csv'
  
 def create_empty_table():
     with open(DimCourse, 'w', newline='') as WorkingFolder:
@@ -92,6 +95,7 @@ def create_empty_table():
 if __name__ == '__main__':
     t1 = datetime.now()
     print('Creating data...')
+    create_csv_file_Transaction_Info()
     create_empty_table()
     t2 = datetime.now()
     print(f"Done in time {t2-t1}")
