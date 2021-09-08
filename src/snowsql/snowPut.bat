3c8cf247c39b21fd14f7a9659088a68b5d540925
Anh Huy Ha <82970821+huyanh0720@users.noreply.github.com>
2021-09-08 15:40:17 -0500

    SSIS Batch Connection Param
    - Create new Param Batch Connection

--- src/snowsql/snowPut.bat
+++ src/snowsql/snowPut.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\Snowflake SnowSQL\snowsql" -c hank0720 -f C:\Users\Admin\Desktop\RunFile\ssisPut.sql
+"C:\Program Files\Snowflake SnowSQL\snowsql" -c hank0720 -f C:\Users\17737\Desktop\Project_Group1_education-Group02\src\RunFile\ssisPut.sql
 
 
 
